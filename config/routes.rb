c022ffd2c54d6ea3782eb4a8043faf4f440c6bf2
Genericsrush <macray607@hotmail.com>
2020-02-27 09:37:04 -0600

    Removed dog routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
   get 'country_origin/index'
   get 'country_origin/show'
   get 'cat_breed/index'
   get 'cat_breed/show'
-  get 'dog_breed/index'
-  get 'dog_breed/show'
   get 'welcome/index'
-  get 'dog/show'
-  get 'dog/index'
   get 'cat/show'
   get 'cat/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
