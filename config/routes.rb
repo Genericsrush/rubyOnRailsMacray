372462c47e49cd58ed2226151a6567dbacf87ffa
Genericsrush <macray607@hotmail.com>
2020-02-27 16:13:33 -0600

    Added appropriate routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,13 +3,13 @@
 Rails.application.routes.draw do
   root to: 'welcome#index'
   get 'temperment/index'
-  get 'temperment/show'
+  get 'temperament/:id', to: 'temperment#show', id: /\d+/
   get 'country_origin/index'
-  get 'country_origin/show'
+  get 'country_origin/:id', to: 'country_origin#show'
   get 'cat_breed/index'
-  get 'cat_breed/show'
+  get 'cat_breed/:id', to: 'cat_breed#show', id: /\d+/
   get 'welcome/index'
-  get 'cat/show'
+  get 'cat/:id', to: 'cat#show', id: /\d+/
   get 'cat/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
