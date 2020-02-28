02012e12e07f9a25f6402b57aa705dbb97de3d96
Genericsrush <macray607@hotmail.com>
2020-02-27 22:20:39 -0600

    Added Cat breed controller functionality

--- app/controllers/cat_breed_controller.rb
+++ app/controllers/cat_breed_controller.rb
@@ -1,7 +1,11 @@
+# frozen_string_literal: true
+
 class CatBreedController < ApplicationController
   def index
+    @cats = Cat.all
   end
 
   def show
+    @cat = Cat.find(:id)
   end
 end
--- app/models/country_origin.rb
+++ app/models/country_origin.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class CountryOrigin < ApplicationRecord
+  has_many :cat_breed
   validates :name, presence: true
   validates :abbriviation, presence: true
 end
